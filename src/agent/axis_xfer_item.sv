a4c00b0400ba5289b0e1bf9c83968a3fd406d4d1
eminakgun <eminakgun96@gmail.com>
2022-07-16 16:10:34 +0300

    Update on axis_xfer_item - Added delay value(source_data_delay) field to for data lines which is used in master_bfm. This value is constrainted to be less than or equal to tvalid_delay by defauly - Added default constraints for deassert_* to be 1'b1 and toggle_tready fields to be 1'b0 - Updated object_params macro to include auxiliary control fields

--- src/agent/axis_xfer_item.sv
+++ src/agent/axis_xfer_item.sv
@@ -15,9 +15,10 @@ class axis_xfer_item #(uint `BUS_WIDTH_PARAMS) extends uvm_sequence_item;
 
   // control
   rand axi_xfer_byte_type_e byte_type[KEEP_STRB_W-1:0];
-  bit xfer_succeed;
+  bit xfer_result;
 
   // master bfm
+  rand uint source_data_delay;
   rand uint tvalid_delay;
   rand bit deassert_tvalid;
 
@@ -29,11 +30,21 @@ class axis_xfer_item #(uint `BUS_WIDTH_PARAMS) extends uvm_sequence_item;
   rand bit toggle_tready;
   rand uint toggle_delay[3];
 
+  constraint always_deassert_c {
+    deassert_tvalid == 1'b1;
+    deassert_tready == 1'b1;
+  }
+
+  constraint disable_tready_toggle_c {
+    toggle_tready == 1'b0;
+  }
+
   constraint valid_ready_common_c {
-    tvalid_delay inside {[0:10]};
+    tvalid_delay inside {[1:10]};
+    source_data_delay <= tvalid_delay; 
     tready_delay inside {[1:10]};
     foreach(toggle_delay[i])
-    toggle_delay[i] inside {[0:10]};
+      toggle_delay[i] inside {[0:10]};
   }
 
   constraint disable_illegal_byte_c {
@@ -54,13 +65,21 @@ class axis_xfer_item #(uint `BUS_WIDTH_PARAMS) extends uvm_sequence_item;
   `uvm_object_new
 
   `uvm_object_param_utils_begin(axis_xfer_item #(`BUS_WIDTH_PARAMS))
-    `uvm_field_int(tdata,  UVM_DEFAULT)
-    `uvm_field_int(tstrb,  UVM_DEFAULT)
-    `uvm_field_int(tkeep,  UVM_DEFAULT)
-    `uvm_field_int(tid,    UVM_DEFAULT)
-    `uvm_field_int(tdest,  UVM_DEFAULT)
-    `uvm_field_int(tuser,  UVM_DEFAULT)
-    `uvm_field_int(tlast,  UVM_DEFAULT)
+    `uvm_field_int(tdata,                 UVM_DEFAULT)
+    `uvm_field_int(tstrb,                 UVM_DEFAULT)
+    `uvm_field_int(tkeep,                 UVM_DEFAULT)
+    `uvm_field_int(tid,                   UVM_DEFAULT)
+    `uvm_field_int(tdest,                 UVM_DEFAULT)
+    `uvm_field_int(tuser,                 UVM_DEFAULT)
+    `uvm_field_int(tlast,                 UVM_DEFAULT)
+    `uvm_field_int(xfer_result,           UVM_DEFAULT)
+    `uvm_field_int(source_data_delay,     UVM_DEFAULT)
+    `uvm_field_int(tvalid_delay,          UVM_DEFAULT)
+    `uvm_field_int(deassert_tvalid,       UVM_DEFAULT)
+    `uvm_field_int(tready_delay,          UVM_DEFAULT)
+    `uvm_field_int(deassert_tready,       UVM_DEFAULT)
+    `uvm_field_int(wait_tvalid_asserted,  UVM_DEFAULT)
+    `uvm_field_int(toggle_tready,         UVM_DEFAULT)
   `uvm_object_utils_end
 
 endclass
